de82bb949b67356fbaea5e55544f5ecfa06c43f6
skadefar <tobias.j.hansen@gmail.com>
2020-01-29 13:49:03 +0100

    projekt oprettet

---
+++ TV2_NFC/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace TV2_NFC.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
